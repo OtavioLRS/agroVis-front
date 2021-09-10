edbed1bd83b86219fc99bb2b2fbaaa34f7446191
OtavioLRSo <otavioleitesantos@hotmail.com>
2021-09-09 21:32:36 -0300

    jiklggij

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,18 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        exclude: /node_modules/
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
       }
     ]
   },
