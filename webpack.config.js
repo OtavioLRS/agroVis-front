3cb90849cf0b7d718c6dd49011f7654698958530
OtavioLRSo <otavioleitesantos@hotmail.com>
2021-09-09 23:23:02 -0300

    n sei

--- webpack.config.js
+++ webpack.config.js
@@ -14,18 +14,7 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader"
-          }
-        ]
+        exclude: /node_modules/
       }
     ]
   },
